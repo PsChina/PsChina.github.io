3b18fbf7669f1589a3b2e5331ec594912500b8d1
PsChina <panshanzxcv.love@163.com>
2018-04-06 19:00:13 +0800

    去除惯性

--- src/components/routes/ArticleList.js
+++ src/components/routes/ArticleList.js
@@ -5,33 +5,24 @@ class List extends React.Component{
    constructor(props){
        super(props)
        this.state = {
-           startTime:0,
            startY:0,
            distansY:0,
            top:0,
-           shuoldChange:0,
-           lastTime:0,
-           duration:0
+           shuoldChange:0
        }
 
    }
    handleStart(event){
        clearInterval( this.state.interval )
         this.setState({
-            startY:event.touches[0].pageY,
-            startTime:new Date().getTime()
+            startY:event.touches[0].pageY
         })
    }
    handleMove(event){
         let shuoldChange = (this.state.startY-event.touches[0].pageY)*1.2
-        const currentTime = new Date().getTime()
         this.setState({
             distansY:event.touches[0].pageY-this.state.startY,
             shuoldChange:Math.abs(shuoldChange) >= 6 ? shuoldChange : 0,
-            duration:currentTime-this.state.lastTime
-        })
-        this.setState({
-            lastTime:currentTime
         })
 
         this.refs.content.scrollTop = this.state.top + this.state.shuoldChange
@@ -40,27 +31,6 @@ class List extends React.Component{
         this.setState({
             top:this.refs.content.scrollTop,
         })
-
-        if( this.state.duration<100&& this.state.lastTime-this.state.startTime>50){
-            const interval = setInterval( ()=>{
-                if(this.state.shuoldChange>6){
-                    this.refs.content.scrollTop += 5+this.state.shuoldChange/this.state.duration;
-                }else if(this.state.shuoldChange<6){
-                    this.refs.content.scrollTop += -5+this.state.shuoldChange/this.state.duration;
-                }
-                this.setState({
-                    top:this.refs.content.scrollTop,
-                })
-            } , this.state.duration/10)
-            this.setState({
-                interval
-            })
-            setTimeout( ()=>{
-                clearInterval(interval)
-            },160+ this.state.shuoldChange*10/this.state.duration)
-        }  
-        
-
    }
    render(){
        return(
@@ -69,7 +39,7 @@ class List extends React.Component{
            onTouchMove={this.handleMove.bind(this)} 
            onTouchEnd={this.handleEnd.bind(this)}
            style={{flex:9,overflowY:'scroll'}}
-           >
+           >    
                <DataStructure/>
            </div>
        )
