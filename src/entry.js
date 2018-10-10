124610c74fe5c63f0c3dceee5d3d4efaec661416
shanshan_pan <shanshan_pan@kingdee.com>
2018-10-10 18:14:26 +0800

    上线贪吃蛇

--- src/entry.js
+++ src/entry.js
@@ -8,6 +8,8 @@ import ArticleList from './components/routes/ArticleList'
 import rem from './rem/rem'
 import isMobile from './is/mobile'
 import ReactAppDemo from './ReactAppDemo/app.js'
+import TanChiShe from './games/tanChiShe/app.jsx'
+
 if(isMobile){
     rem(3.2);
     document.ontouchmove = function(e){ // 阻止向下翻页时 网页向下滑
@@ -41,9 +43,10 @@ class App extends React.Component {
                 <AppHeader/>
                 <Switch>
                     <Redirect path="/" component={Home} to="/home" exact></Redirect>
-                    <Route path="/home" component={Home}></Route>
-                    <Route path="/list" component={ArticleList}></Route>
-                    <Route path="/react/homework" component={ReactAppDemo}></Route>   
+                    <Route path="/home" component={Home} />
+                    <Route path="/list" component={ArticleList} />
+                    <Route path="/react/homework" component={ReactAppDemo} />
+                    <Route path="/games/tcs" component={TanChiShe} />
                 </Switch>
             </div>
         )
---
+++ src/games/tanChiShe/app.jsx
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+import './css/snakeStyle.css'
+import main from './js/snake.js'
+class App extends Component{
+	constructor(){
+		super()
+	}
+	componentDidMount(){
+		main()
+	}
+	render(){
+		return (
+			<div className="gameBox">
+				<div className="snake">
+					<div className="snakeHead"></div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default App
