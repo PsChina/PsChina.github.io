82e7c1fe47854312c4681fa5be2dfdfbc0ddfaf7
PsChina <panshanzxcv.love@163.com>
2018-11-21 18:28:11 +0800

    </div>

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -9,6 +9,7 @@ class Home extends React.Component{
            <div style={{flex:9,overflowY:'scroll'}}>
                 <h1>Home</h1>
                 <a href="http://www.feieyun.com/open/index.html">飞蛾打印机开放平台</a>
+                <div>fullter swift kotlin react</div>
            </div>
        )
    }
